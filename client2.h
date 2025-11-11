3d6e6d7559f83a037cbb1201528e469fdd93b847
robin83470 <robin.renous83@gmail.com>
2025-11-12 00:31:13 +0100

    aef

--- client2.h
+++ client2.h
@@ -17,7 +17,7 @@ typedef struct
    SOCKET sock;
    char name[50];
    Game game;
-   int etat; //0 rien, 1 cherche une partie, 2 joue et à son tour, 3 joue et pas son tour, 4 spectateur, 5 doit entrer un spectateur, 6 doit donner coup, 7 doit chatter et à tour, 8 chat et pas à son tour, 9 défié en attente, 10 defi recu, 11 en attente de reponse de defi
+   int etat; //0 rien, 1 cherche une partie, 2 joue et à son tour, 3 joue et pas son tour, 4 spectateur, 5 doit entrer un spectateur, 6 doit donner coup, 7 doit chatter et à tour, 8 chat et pas à son tour, 9 défié en attente, 10 defi recu, 11 en attente de reponse de defi, 12 envoyer un message en temps que spectateur
    char namespec[BUF_SIZE];
 }Client;
 
